d613005a2f85be5845c754aecaf7ca879acb873e
achmad jamaludin <achmad.jamaludin@online-pajak.com>
2022-11-21 20:00:15 +0700

    delete generated folder

--- dist/src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const game_1 = require("./services/game");
-const numberOfPlayer = process.argv.slice(2);
-const game = new game_1.Game(parseInt(numberOfPlayer[0]));
-if (!game.validate())
-    throw new Error('Too many players, please input max 10 player');
-game.play();
